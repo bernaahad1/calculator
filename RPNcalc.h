10975c6b4c4525c63a8e788a8d98ee209f360238
bernaahad1 <75627220+bernaahad1@users.noreply.github.com>
2021-01-09 21:24:15 +0200

    Created header file for the functions that calculate the postfix expression.

---
+++ RPNcalc.h
@@ -0,0 +1,92 @@
+#include <stack>
+
+using namespace std;
+
+bool isOperand(const char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+bool isOperator(const char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+
+void doOperation(const double leftOp, const double rightOp, const char ch, double& result) {
+    switch (ch) {
+    case '+':
+        result = leftOp + rightOp;
+        return;
+    case '-':
+        result = leftOp - rightOp;
+        return;
+    case '*':
+        result = leftOp * rightOp;
+        return;
+    case '/':
+        result = leftOp / rightOp;
+        return;
+    case '^':
+        result = pow(leftOp, rightOp);
+        return;
+    }
+    return;
+}
+
+void calculate(const string& expression, double& result) {
+    stack <double> stack;
+
+    for (size_t i = 0; i < expression.size(); i++) {
+
+        if (isOperand(expression[i])) {
+            double operand = (double)(expression[i]) - '0';
+
+            while (expression[i + 1] != ' ') {
+                operand = operand * 10 + ((double)(expression[i + 1]) - '0');
+
+                ++i;
+            }
+
+            stack.push(operand);
+        }
+        else if (expression[i] == '-' && isOperand(expression[i + 1])) {
+            double operand = ((double)(expression[i + 1]) - '0');
+            ++i;
+
+            while (expression[i + 1] != ' ') {
+                operand = operand * 10 + ((double)(expression[i + 1]) - '0');
+                ++i;
+            }
+
+            operand *= (-1);
+            stack.push(operand);
+        }
+        else if (isOperator(expression[i])) {
+            double leftOp = 0;
+            double rightOp = 0;
+            double curResult = 0;
+            rightOp += stack.top();
+            stack.pop();
+
+            if ((!stack.empty()) && stack.top() != '#') {
+                leftOp += stack.top();
+                stack.pop();
+            }
+
+            doOperation(leftOp, rightOp, expression[i], curResult);
+
+            if (curResult == INFINITY) {
+                cout << "NaN" << endl;
+                return;
+            }
+            if (rightOp == 0 && expression[i] == '/') {
+                cout << "NaN" << endl;
+                return;
+            }
+
+            stack.push(curResult);
+        }
+    }
+
+    result = stack.top();
+    cout << result << endl;
+    return;
+}
